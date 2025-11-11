d631148d8cbe8c0acc91f795ca15861fb0aeb8fc
Faarae <ghffr64@gmail.com>
2025-11-11 19:40:58 +0700

    first commit

---
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -0,0 +1,15 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import com.example.todoapp.ui.theme.TodoScreen
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                TodoScreen()
+            }
+        }
+    }
+}
